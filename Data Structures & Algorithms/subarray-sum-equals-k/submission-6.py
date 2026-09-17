f85c9a16daba1682ab64f8a4b2c7ef8798c98b72
Arno Demarteau <arno.demarteau@gmail.com>
2026-09-17 00:13:04 -0700

    Add: subarray-sum-equals-k - submission-6

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        # At every position, what previous prefix sum would make my current subarray equal k?
+        count_equals_k = 0
+        prefixSum = 0
+        seen = {0: 1} # Prefix sum before the array
+
+        for num in nums:
+            prefixSum += num
+            if prefixSum - k in seen:
+                count_equals_k += seen[prefixSum - k]
+                
+            if prefixSum in seen:
+                seen[prefixSum] += 1
+            else:
+                seen[prefixSum] = 1
+
+        return count_equals_k
